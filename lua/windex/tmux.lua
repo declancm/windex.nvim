17441f148ad4011b55eaccc95ffd48c321b33c2e
declancm <declanmullen3.14@gmail.com>
2022-04-03 20:29:53 +1000

    Use getenv for tmux check.

--- lua/windex/tmux.lua
+++ lua/windex/tmux.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.requirement_passed = function()
-  -- Check if command fails. If it fails, tmux is not installed.
-  if os.execute('tmux -V') ~= 0 then
+  -- Check if TMUX is installed.
+  if not os.getenv('TMUX') then
     return false
   end
   -- Check if the version number is 1.8 or greater (for 'resize-pane -Z').
--- lua/windex/utils.lua
+++ lua/windex/utils.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.error_msg = function(message, code, color)
+  message = vim.fn.escape(message, '"\\')
   code = code or 'Error'
   color = color or 'ErrorMsg'
   vim.cmd(string.format('echohl %s | echom "%s: %s" | echohl None', color, code, message))
